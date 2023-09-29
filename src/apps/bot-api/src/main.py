23871894698fd11830a24be15b00f846dd10c657
Julien Kmec <me@julien.dev>
2023-09-28 21:32:35 -0400

    fix

--- src/apps/bot-api/src/main.py
+++ src/apps/bot-api/src/main.py
@@ -1,9 +1,10 @@
-import asyncio
 import importlib
 import logging
 import os
+from functools import partial
 
 from sanic import Sanic
+from sanic.worker.loader import AppLoader
 
 import resources.database as database
 from config import DEBUG_MODE, SERVER_HOST, SERVER_PORT
@@ -12,22 +13,31 @@ from middleware import auth
 logging.basicConfig()
 
 
-def register_routes(path=None):
+def register_routes(app, path=None):
     path = path or ["src/routes"]
     files = os.listdir("/".join(path))
 
     for file_or_folder in files:
         if "__" not in file_or_folder:
             if os.path.isdir(f"{'/'.join(path)}/{file_or_folder}"):
-                register_routes(path + [f"{file_or_folder}"])
+                register_routes(app, path + [f"{file_or_folder}"])
             else:
                 proper_path = "/".join(path) + "/" + file_or_folder
-                import_name = proper_path.replace("/", ".").replace(".py", "").replace("src.", "")
+                import_name = proper_path.replace(
+                    "/", ".").replace(".py", "").replace("src.", "")
 
                 route_module = importlib.import_module(import_name)
                 route = getattr(route_module, "Route")()
 
-                app.add_route(getattr(route, "handler"), getattr(route, "PATH"), getattr(route, "METHODS"))
+                app.add_route(getattr(route, "handler"), getattr(
+                    route, "PATH"), getattr(route, "METHODS"))
+
+
+def create_app() -> Sanic:
+    app = Sanic("BloxlinkBotAPIServer")
+    register_routes(app)
+
+    return app
 
 
 async def main():
@@ -37,9 +47,10 @@ async def main():
 
 
 if __name__ == "__main__":
-    app = Sanic("BloxlinkBotAPIServer")
+    loader = AppLoader(factory=partial(create_app))
+
+    app = loader.load()
     app.register_middleware(auth, "request")
+    app.prepare(host=SERVER_HOST, port=SERVER_PORT, dev=True)
 
-    asyncio.run(main())
-
-    app.run(SERVER_HOST, SERVER_PORT, fast=not DEBUG_MODE, debug=DEBUG_MODE, access_log=DEBUG_MODE)
+    Sanic.serve(primary=app, app_loader=loader)

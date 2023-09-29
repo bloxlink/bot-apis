c82ba03f757ce97a1a1e91bc0226a527b5ac806c
Julien Kmec <me@julien.dev>
2023-09-28 21:14:02 -0400

    chatgpt modified run script

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import subprocess
 import signal
+import os
 
 APPS = {
     "roblox-info-server": "python3.10 src/main.py",
@@ -22,8 +23,30 @@ signal.signal(signal.SIGINT, terminate_processes)
 # Start all apps
 for app_name, app_run_command in APPS.items():
     process = subprocess.Popen(
-        f"cd src/apps/{app_name}; {app_run_command}", shell=True)
+        f"cd src/apps/{app_name}; {app_run_command}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
     processes.append(process)
 
+# Continuously check and print errors if any
+while processes:
+    for process in processes:
+        retcode = process.poll()
+        if retcode is not None:
+            stdout, stderr = process.communicate()
+            if retcode != 0:
+                print(f"Error occurred in {app_name}:")
+                print("Standard Output:")
+                print(stdout)
+                print("Standard Error:")
+                print(stderr)
+            else:
+                print(f"{app_name} completed successfully.")
+            processes.remove(process)
+
+# Ensure all processes are terminated before exiting
 for process in processes:
-    process.wait()
+    process.terminate()

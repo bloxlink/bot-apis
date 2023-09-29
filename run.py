f63b65d7a22f216f27590adc058c08b4051f1d2b
Julien Kmec <me@julien.dev>
2023-09-28 20:51:17 -0400

    Run file

---
+++ run.py
@@ -0,0 +1,16 @@
+import subprocess
+
+APPS = {
+    "roblox-info-server": "python3.10 src/main.py",
+    "bot-api": "python3.10 src/main.py",
+    "discord-gateway-relay": "python3.10 src/main.py"
+}
+
+processes = []
+
+for app_name, app_run_command in APPS.items():
+    process = subprocess.Popen(f"cd src/apps/{app_name}; {app_run_command}", shell=True)
+    processes.append(process)
+
+for process in process:
+    process.wait()

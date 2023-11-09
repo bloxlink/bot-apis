1d7c86f2ce7607ceb15f2d7211ba1a5509257ad4
Nub <38899321+One-Nub@users.noreply.github.com>
2023-11-09 19:58:34 +0000

    Comment that middleware auth checking is not working

--- src/apps/bot-api/src/middleware.py
+++ src/apps/bot-api/src/middleware.py
@@ -3,6 +3,7 @@ from sanic.response import text
 from resources.secrets import BIND_API_AUTH
 
 
+# TODO: This isn't being checked.
 async def auth(request):
     if request.headers.get("Authorization") != BIND_API_AUTH:
         return text("Unauthorized", status=401)

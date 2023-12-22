0a3885c51b911823b47b0016238e0cab1ff863f9
Nub <38899321+One-Nub@users.noreply.github.com>
2023-12-22 22:14:40 +0000

    Use enumerate instead of len(range())

--- src/apps/discord-gateway-relay/src/resources/endpoints/verifyall.py
+++ src/apps/discord-gateway-relay/src/resources/endpoints/verifyall.py
@@ -4,10 +4,10 @@ import math
 
 from discord import Forbidden, Guild, HTTPException
 
-from resources.secrets import BIND_API_AUTH, BIND_API
 from resources.ipc import RelayEndpoint, RelayRequest
 from resources.modules.discord import client
 from resources.modules.redis import redis_connection
+from resources.secrets import BIND_API, BIND_API_AUTH
 from resources.utils import MinimalConversions, ReturnType, fetch
 
 
@@ -52,45 +52,37 @@ class VerifyallEndpoint(RelayEndpoint):
 
         # Needs to be done or the redis response will timeout (for some reason) and only the first
         # chunk will be sent.
-        asyncio.create_task(self._handle_chunks(
-            guild, channel_id, chunk_limit))
+        asyncio.create_task(self._handle_chunks(guild, channel_id, chunk_limit))
 
     async def _handle_chunks(self, guild: Guild, channel_id, chunk_limit):
         members = guild.members
         if not guild.chunked:
-            logging.debug(f"Chunking the members of the guild.")
+            logging.debug("Chunking the members of the guild.")
             members = await guild.chunk()
 
         cooldown_key = f"guild_scan:{guild.id}"
         await redis_connection.set(cooldown_key, "2", ex=86400)
 
-        logging.debug(
-            f"Splitting the chunked guild members into smaller chunks.")
-        split_chunk = [members[i: i + chunk_limit]
-                       for i in range(0, len(members), chunk_limit)]
+        logging.debug(f"Splitting the chunked guild members into smaller chunks.")
+        split_chunk = [members[i : i + chunk_limit] for i in range(0, len(members), chunk_limit)]
 
-        for i in range(len(split_chunk)):
+        for i, chunk in enumerate(split_chunk):
             is_final = i == len(split_chunk) - 1
-
-            chunk = split_chunk[i]
             chunk = [MinimalConversions.convert_member(x) for x in chunk]
 
-            logging.debug(
-                f"Sending chunk {i + 1} of {len(split_chunk)} chunks.")
+            logging.debug(f"Sending chunk {i + 1} of {len(split_chunk)} chunks.")
             text, response = await fetch(
                 "POST",
                 f"{BIND_API}/api/update/users",
                 headers={"Authorization": BIND_API_AUTH},
-                body={"guild_id": guild.id, "channel_id": channel_id,
-                      "members": chunk, "is_done": is_final},
+                body={"guild_id": guild.id, "channel_id": channel_id, "members": chunk, "is_done": is_final},
                 return_data=ReturnType.TEXT,
                 timeout=None,
             )
             logging.debug(f"BOT SERVER RESPONSE: {response.status}, {text}")
 
             if response.status > 400:
-                logging.error(
-                    f"BOT SERVER RESPONSE: {response.status}, {text}")
+                logging.error(f"BOT SERVER RESPONSE: {response.status}, {text}")
                 break
 
             # Wait 3 seconds before sending the next request.

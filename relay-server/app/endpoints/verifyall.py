352ba4c4bce95660f08deaa3576aac33880d8639
Julien Kmec <me@julien.dev>
2024-02-18 19:06:51 -0500

    add more attributes to response

--- relay-server/app/endpoints/verifyall.py
+++ relay-server/app/endpoints/verifyall.py
@@ -22,11 +22,13 @@ class Payload(BaseModel):
 
 class VerifyAllProgress(BaseModel):
     started: datetime
-    progress: int
-    total: int
+    members_processed: int
+    total_members: int
+    current_chunk: int
+    total_chunks: int
 
 
-async def record_progress(nonce: str, progress: int, total: int):
+async def record_progress(nonce: str, progress: int, total: int, current_chunk: int, total_chunks: int):
     """Record the progress of the verification."""
 
     current_progress: dict = json.loads(await redis.get(f"progress:{nonce}")) if await redis.exists(f"progress:{nonce}") else {}
@@ -34,14 +36,17 @@ async def record_progress(nonce: str, progress: int, total: int):
     if not current_progress:
         current_progress = {
             "started": datetime.now(),
-            "progress": progress,
-            "total": total
+            "members_processed": progress,
+            "total_members": total,
+            "current_chunk": current_chunk,
+            "total_chunks": total_chunks
         }
 
     parsed_progress = parse_into(current_progress, VerifyAllProgress)
+    parsed_progress.members_processed = progress + parsed_progress.members_processed
+    parsed_progress.current_chunk = current_chunk
+    parsed_progress.total_chunks = total_chunks
 
-    parsed_progress.progress = progress
-    parsed_progress.total = total
 
     await redis.set(f"progress:{nonce}", parsed_progress.model_dump_json(), ex=int(timedelta(days=2).total_seconds()))
 
@@ -56,12 +61,12 @@ class VerifyAllEndpoint(RelayEndpoint[Payload]):
     async def handle_chunks(self, guild: discord.Guild, members: list[discord.Member], chunk_limit: int, nonce: str):
         """Handle the chunking of the members."""
 
-        await record_progress(nonce, 0, len(members))
+        await record_progress(nonce, 0, len(members), 0, 0)
 
         split_chunk = [members[i : i + chunk_limit] for i in range(0, len(members), chunk_limit)]
 
-        for member_chunk in split_chunk:
-            pass
+        for i, member_chunk in enumerate(split_chunk, 1):
+            await record_progress(nonce, len(member_chunk), len(members), i, len(split_chunk))
 
     async def handle(self, request: RedisRelayRequest[Payload]) -> Response:
         payload = request.payload

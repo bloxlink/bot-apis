c55d1d6d961c3064349284f6a739ff48adf25f84
Julien Kmec <me@julien.dev>
2024-02-19 22:49:53 -0500

    cancel scan

--- relay-server/app/endpoints/verifyall.py
+++ relay-server/app/endpoints/verifyall.py
@@ -25,7 +25,7 @@ class Payload(BaseModel):
 
 class VerifyAllProgress(BaseModel):
     started_at: datetime
-    ended_at: datetime = None
+    ended_at: datetime | None = None
     members_processed: int
     total_members: int
     current_chunk: int
@@ -51,12 +51,13 @@ async def record_progress(nonce: str, progress: int, total: int, current_chunk: 
     parsed_progress.current_chunk = current_chunk
     parsed_progress.total_chunks = total_chunks
 
-    if total_chunks == current_chunk:
+    if total_chunks == current_chunk and total_chunks != 0:
         parsed_progress.ended_at = datetime.now()
-
 
     await redis.set(f"progress:{nonce}", parsed_progress, expire=timedelta(days=2))
 
+    if await redis.get(f"progress:{nonce}:cancelled"):
+        raise asyncio.CancelledError
 
 
 class VerifyAllEndpoint(RelayEndpoint[Payload]):
@@ -68,35 +69,40 @@ class VerifyAllEndpoint(RelayEndpoint[Payload]):
     async def handle_chunks(self, guild: discord.Guild, members: list[discord.Member], chunk_limit: int, nonce: str):
         """Handle the chunking of the members."""
 
-        await record_progress(nonce, 0, len(members), 0, 0)
-
         split_chunk = [members[i : i + chunk_limit] for i in range(0, len(members), chunk_limit)]
 
-        for i, member_chunk in enumerate(split_chunk, 1):
-            logging.debug(f"Sending chunk {i + 1} of {len(split_chunk)} chunks.")
+        await record_progress(nonce, 0, len(members), 0, len(split_chunk))
 
-            text, response = await fetch(
-                "POST",
-                f"{CONFIG.HTTP_BOT_API}/api/update/users",
-                headers={"Authorization": CONFIG.HTTP_BOT_AUTH},
-                body={
-                    "guild_id": guild.id,
-                    "members": [MemberSerializable.from_discordpy(m).model_dump() for m in member_chunk],
-                },
-                parse_as="JSON",
-                timeout=None,
-            )
-            logging.debug(f"BOT SERVER RESPONSE: {response.status}, {text}")
+        try:
+            for i, member_chunk in enumerate(split_chunk, 1):
+                logging.debug(f"Sending chunk {i + 1} of {len(split_chunk)} chunks.")
 
-            if response.status != StatusCodes.OK:
-                logging.error(f"Verify endpoint response: {response.status}, {text}")
-                break
+                text, response = await fetch(
+                    "POST",
+                    f"{CONFIG.HTTP_BOT_API}/api/update/users",
+                    headers={"Authorization": CONFIG.HTTP_BOT_AUTH},
+                    body={
+                        "guild_id": guild.id,
+                        "members": [MemberSerializable.from_discordpy(m).model_dump() for m in member_chunk],
+                    },
+                    parse_as="JSON",
+                    timeout=None,
+                    raise_on_failure=False
+                )
+                logging.debug(f"BOT SERVER RESPONSE: {response.status}, {text}")
 
-            # Wait 3 seconds before sending the next request.
-            logging.debug("Verify endpoint: sleeping for 3 seconds.")
-            await asyncio.sleep(3)
+                if response.status != StatusCodes.OK:
+                    logging.error(f"Verify endpoint response: {response.status}, {text}")
+                    break
 
-            await record_progress(nonce, len(member_chunk), len(members), i, len(split_chunk))
+                # Wait 3 seconds before sending the next request.
+                logging.debug("Verify endpoint: sleeping for 3 seconds.")
+                await asyncio.sleep(3)
+
+                await record_progress(nonce, len(member_chunk), len(members), i, len(split_chunk))
+
+        except asyncio.CancelledError:
+            return
 
     async def handle(self, request: RedisRelayRequest[Payload]) -> Response:
         payload = request.payload
